c3f4556b130f853ae1a02df5c7d5413f07af4de4
Wellington Bonfim <wellington@cappuccinodigital.com.br>
2021-05-25 01:06:06 -0300

    WIP: mexendo no back end

--- bd_script.sql
+++ bd_script.sql
@@ -1,15 +1,15 @@
 
 
-#create database website;
+#create database mr_solis;
 
-create table website.contact (
+create table mr_solis.contact (
     id int(11) AUTO_INCREMENT,
 	name varchar(50) NULL,
 	city varchar(50) NULL,
 	state varchar(30) NULL,
-	mail varchar(30) NULL,
+	mail varchar(100) NULL,
 	consumption varchar(50) NULL,
-	company varchar(50) NULL,
+	company varchar(100) NULL,
 	is_send_mail boolean NULL,
 	created_at datetime NOT NULL,
     PRIMARY KEY (id)
--- service/ContactService.php
+++ service/ContactService.php
@@ -1,5 +1,9 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 class ContactService {
     
     private $em;
@@ -7,7 +11,7 @@ class ContactService {
 
     public function __construct() 
     {
-        $this->em  = new mysqli('localhost', 'root', '', 'website');
+        $this->em  = new mysqli('localhost', 'root', '', 'mr_solis');
     }
 
     public function conection() 
@@ -32,7 +36,7 @@ class ContactService {
         $mail = isset($request['mail']) ? $request['mail'] : null;
         $consumption = isset($request['consumption']) ? $request['consumption'] : null;
         $company = isset($request['company']) ? $request['company'] : null;
-        $is_send_mail = true;
+        $is_send_mail = false;
         
         $error = [];
         if ($token) {
@@ -71,9 +75,17 @@ class ContactService {
             $response = ['status' => false, 'message' => 'Campos obrigatórios.'];
             return $error;
         }
-                
+
+        //Disparo de e-mail
+        $send_mail = $this->sendMail($request); 
+
+        if ($send_mail['status']) {
+            $is_send_mail = true;
+        }
+       
+        $date = date('Y-m-d h:i:s');
         $sql = "INSERT INTO contact (name, city, state, mail, consumption, company, is_send_mail, created_at)
-                    VALUES ('$name', '$city', '$state', '$mail', '$consumption' , '$company', $is_send_mail, '2021-05-08 03:27:05');";
+                    VALUES ('$name', '$city', '$state', '$mail', '$consumption' , '$company', $is_send_mail, '{$date}');";
         
         if ($this->em->query($sql) === true) {
             $response = ['status' => true];
@@ -84,8 +96,52 @@ class ContactService {
         return $response;
     }  
 
-    public function sendMail() 
+    public function sendMail($request) 
     {
-        
+        //Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //configuração e-mail
+            //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                   
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.gmail.com';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = 'mrsolis.eletrica@gmail.com';
+            $mail->Password   = 'm&rsolis2021';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port       = 587;
+
+            //Recipients
+            $mail->setFrom('mrsolis.eletrica@gmail.com', 'MR Solis');       
+            $mail->addAddress('mrsolis.eletrica@gmail.com', 'MR Solis');  //Adicionando um destinatário, (o nome é opcional)
+            $mail->addReplyTo($request['mail'], $request['name']);
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
+
+            //Anexo     
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg'); //O nome é opcional
+
+            $body = '<h1>Informações de Contato</h1>';
+            $body .= '<strong>Nome: </strong> ' . $request['name'] . '<br>';
+            $body .= '<strong>Cidade: </strong>' . $request['city'] . '<br>';
+            $body .= '<strong>Estado: </strong>' . $request['state'] . '<br>';
+            $body .= '<strong>E-mail: </strong>' . $request['mail'] . '<br>';
+            $body .= '<strong>Consumo Mensal: </strong>' . $request['consumption'] . '<br>';
+            $body .= '<strong>Concessionária de Energia: </strong> ' . $request['company'] . '<br>';
+            
+            $mail->CharSet = 'UTF-8';
+            $mail->isHTML(true);
+            $mail->Subject = 'Simulação Eletrica - Site';
+            $mail->Body    = $body;
+            //$mail->AltBody = '';
+
+            $mail->send();
+            $response = ['status' => true, 'message' => 'success'];
+        } catch (Exception $e) {
+            $response = ['status' => false, 'message' => 'Ops! E-mail não enviado, tente novamente mais tarde.'];
+        }
+
+        return $response;
     }
 }
